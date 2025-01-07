bd5cadb60ba82adf6666c2bda6250621203c1c43
Brianna O'Regan <brianna.oregan@stu.ocsb.ca>
2025-01-07 15:34:33 +0000

    added random select word & display it as blanks

--- hangman.py
+++ hangman.py
@@ -8,3 +8,11 @@ word_bank = ["apple", "horror", "illusion", "obstacle", "conclusion"]
 
 # randomly selects word from word bank
 word_to_guess = random.choice(word_bank)
+
+# displays word as a string of blanks
+display_word = ""
+for _ in word_to_guess:
+    display_word += "_ "
+
+# displays the blanks
+print("Word to guess:", display_word)
