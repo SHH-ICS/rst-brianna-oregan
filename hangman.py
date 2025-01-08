5f23a5f7bd945bc0b19a5732e85d50c7420260bb
Brianna O'Regan <brianna.oregan@stu.ocsb.ca>
2025-01-08 15:49:21 +0000

    added input validation

--- hangman.py
+++ hangman.py
@@ -4,7 +4,7 @@
 import random
 
 # create a list to use as the word bank for the game
-word_bank = ["apple", "horror", "illusion", "obstacle", "conclusion"]
+word_bank = ["apple", "horror", "illusion", "vehicle", "obstacle", "conclusion"]
 
 # randomly selects word from word bank
 word_to_guess = random.choice(word_bank)
@@ -14,5 +14,37 @@ display_word = ""
 for _ in word_to_guess:
     display_word += "_ "
 
-# displays the blanks
-print("Word to guess:", display_word)
+print("Your word:", display_word)
+
+# list to track guessed letters
+guessed_letters = []
+wrong_guesses = []
+
+#Set the # of attempts
+attempts_remaining = 6
+
+# Game loop
+while attempts_remaining > 0 and "_" in display_word:
+    # Display the current state of the word & the game info
+    print("Your word: " + )
+    print()
+    print()
+
+
+
+
+
+#asks the user for a guess + validates it
+while True:
+    # prompts user to guess a letter
+    guess = input("Enter your guess: ").lower()
+
+    # validates input. if invalid it'll loop until a valid guess is entered
+    if len(guess) != 1 or not guess.isalpha():
+        print("Error: Guess must be a single letter.")
+    elif guess in guessed_letters:
+        print("You've already guessed " + str(guess) + ". Try a different letter.")
+    else:
+        #add guess to list of guessed letters
+        guessed_letters = guessed_letters + [guess]
+        break #exit the loop once a valid guess is entered
