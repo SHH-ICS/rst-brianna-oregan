c09a55a7dceaebf1e70f7f5a808b9a5623f914dc
Brianna O'Regan <brianna.oregan@stu.ocsb.ca>
2025-01-09 15:36:50 +0000

    configured what is displayed to the user

--- hangman.py
+++ hangman.py
@@ -1,50 +1,53 @@
 # hangman.py
-# Hangman Game
+# ICD2O Final Project - Hangman Game
 
 import random
 
-# create a list to use as the word bank for the game
+# Stage 1: setup for the game
+# Word bank for the game
 word_bank = ["apple", "horror", "illusion", "vehicle", "obstacle", "conclusion"]
 
-# randomly selects word from word bank
+# Randomly select a word from the word bank
 word_to_guess = random.choice(word_bank)
 
-# displays word as a string of blanks
-display_word = ""
-for _ in word_to_guess:
-    display_word += "_ "
+# List of blanks to represent the word
+display_word = ["_"] * len(word_to_guess)
 
-print("Your word:", display_word)
-
-# list to track guessed letters
+# List to track guessed letters
 guessed_letters = []
 wrong_guesses = []
 
 #Set the # of attempts
 attempts_remaining = 6
 
+# Welcome message
+print("Welcome to Hangman! :)")
+
+# Stages 2: user input
 # Game loop
 while attempts_remaining > 0 and "_" in display_word:
-    # Display the current state of the word & the game info
-    print("Your word: " + )
+    # Stage 3: game logic
+    # DIsplay the current state of the word + game info
+    print("Your word: ", end = "")
+    for char in display_word:
+        print(char, end = "")
     print()
-    print()
-
-
-
-
-
-#asks the user for a guess + validates it
-while True:
-    # prompts user to guess a letter
+    print("You have " + str(attempts_remaining) + " attemps remaining.")
+    print("Incorrect guesses: ", end = "")
+    # Loop thru the indices of the wrong_guesses list
+    for i in range(len(wrong_guesses)):
+        # Check if the current index is not the last index in the list
+        if i < len(wrong_guesses) - 1:
+            # Prints current wrong guess followed by a comma
+            # Uses end to avoid moving to a new line after printing
+            print(wrong_guesses[i] + ", ", end = "")
+        else:
+            # If it's the last index then it prints without a comma after
+            print(wrong_guesses[i])
+    
+    # Ask user to guess a letter
     guess = input("Enter your guess: ").lower()
-
-    # validates input. if invalid it'll loop until a valid guess is entered
-    if len(guess) != 1 or not guess.isalpha():
-        print("Error: Guess must be a single letter.")
-    elif guess in guessed_letters:
-        print("You've already guessed " + str(guess) + ". Try a different letter.")
-    else:
-        #add guess to list of guessed letters
-        guessed_letters = guessed_letters + [guess]
-        break #exit the loop once a valid guess is entered
+    
+    # Validates input
+    #THIS IS WHAT I NEED TO ADD NEXT :D
+    
